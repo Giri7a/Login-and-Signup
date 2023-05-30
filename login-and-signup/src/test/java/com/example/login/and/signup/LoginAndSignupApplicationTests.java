6194b6ad893db42790d17e86d577e0e18eaba66d
Giri7a <110093670+Giri7a@users.noreply.github.com>
2023-05-31 00:11:31 +0530

    Add files via upload

---
+++ login-and-signup/src/test/java/com/example/login/and/signup/LoginAndSignupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login.and.signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndSignupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
